3648765665e06a4354eeb9e4444905b2ef6a3200
Sanvirus <sannijs.bondars123@gmail.com>
2024-11-04 04:42:43 +0200

    did some stuff

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import Header from './components/Header'; // Adjust the path if needed
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 
 function App() {
   return (
-   
-      <div className="App">
-        <Header />
-        {/* Other components here */}
-      </div>
-   
+    <div className="App">
+      <Header />
+      <main>
+        <About />
+        <Projects />
+        <Contact />
+      </main>
+    </div>
   );
 }
 
