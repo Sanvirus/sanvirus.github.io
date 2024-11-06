01d81c6fb9d96264ea05b18225a7b1bcdbc8d2e5
Sanvirus <sannijs.bondars123@gmail.com>
2024-11-06 09:10:12 +0200

    nav-bar

--- my-app/src/components/HeroImg.js
+++ my-app/src/components/HeroImg.js
@@ -1,9 +1,25 @@
 import "./HeroStyle.css";
 import React from 'react'
+import IntroImg from "../assets/jaanni.jpeg"
+import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
-    <div>HeroImg</div>
+    <div className="hero">
+        <div className="mask">
+            <img className="into-img" src={IntroImg} alt="jāņu bilde"/>
+        </div>
+        <div>
+            <div className="content">
+                <p>HI, I'M A STUDENT</p>
+                <h1>PROGRAMMER/ GRAPHICDESIGNER</h1>
+            <div>
+                <Link to="/project" className="btn ">Projects</Link>
+                <Link to="/contact" className="btn btn-light">Contact</Link>
+            </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
