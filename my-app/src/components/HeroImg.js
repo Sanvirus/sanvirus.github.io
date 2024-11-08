67cab9c13b734e089b4679b6d257b0af6acfc075
Sanvirus <sannijs.bondars123@gmail.com>
2024-11-08 19:27:38 +0200

    hero

--- my-app/src/components/HeroImg.js
+++ my-app/src/components/HeroImg.js
@@ -11,8 +11,8 @@ const HeroImg = () => {
         </div>
         <div>
             <div className="content">
-                <p>HI, I'M A STUDENT</p>
-                <h1>PROGRAMMER/ GRAPHICDESIGNER</h1>
+                <p>junior web developer/ Junior graphic designer</p>
+                <h1>"Ja kods neiet, es ieteikšu restartēt. Ja restartēšana nepalīdz, es ķeršos pie risinājuma!"</h1>
                 <div className="hero-btn">
                     <Link to="/project" className="butn">Projects</Link>
                     <Link to="/contact" className="butn">Contact</Link>
