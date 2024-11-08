e398bfcaca3a86434e74a6af4a66555abe0b8b43
Sanvirus <sannijs.bondars123@gmail.com>
2024-11-08 22:35:27 +0200

    hero

--- my-app/src/components/HeroImg.js
+++ my-app/src/components/HeroImg.js
@@ -1,6 +1,6 @@
 import "./HeroStyle.css";
 import React from 'react'
-import IntroImg from "../assets/forest2.jpeg"
+import IntroImg from "../assets/HeroIMG.jpeg"
 import { Link } from "react-router-dom";
 
 const HeroImg = () => {
@@ -12,10 +12,10 @@ const HeroImg = () => {
         <div>
             <div className="content">
                 <p>junior web developer/ Junior graphic designer</p>
-                <h1>"Ja kods neiet, es ieteikšu restartēt. Ja restartēšana nepalīdz, es ķeršos pie risinājuma!"</h1>
+                <h1> "Ja kods neiet, es ieteikšu restartēt. Ja restartēšana nepalīdz, es ķeršos pie risinājuma!"</h1>
                 <div className="hero-btn">
-                    <Link to="/project" className="butn">Projects</Link>
-                    <Link to="/contact" className="butn">Contact</Link>
+                    <Link to="/project" className="btn">Projects</Link>
+                    <Link to="/contact" className="btn btn-light">Contact</Link>
                 </div>
             </div>
         </div>
