32bc4aaaec87e5a981a173770a9a44b822325f09
Sanvirus <sannijs.bondars123@gmail.com>
2024-11-08 15:24:48 +0200

    stuff

--- my-app/src/components/HeroImg.js
+++ my-app/src/components/HeroImg.js
@@ -1,22 +1,22 @@
 import "./HeroStyle.css";
 import React from 'react'
-import IntroImg from "../assets/jaanni.jpeg"
+import IntroImg from "../assets/forest2.jpeg"
 import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
     <div className="hero">
         <div className="mask">
-            <img className="into-img" src={IntroImg} alt="jāņu bilde"/>
+            <img className="into-img" src={IntroImg} alt="mežā"/>
         </div>
         <div>
             <div className="content">
                 <p>HI, I'M A STUDENT</p>
                 <h1>PROGRAMMER/ GRAPHICDESIGNER</h1>
-            <div>
-                <Link to="/project" className="btn ">Projects</Link>
-                <Link to="/contact" className="btn btn-light">Contact</Link>
-            </div>
+                <div className="hero-btn">
+                    <Link to="/project" className="button-49 ">Projects</Link>
+                    <Link to="/contact" className="button-49">Contact</Link>
+                </div>
             </div>
         </div>
     </div>
