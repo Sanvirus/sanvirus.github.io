f7733d00e52825fc824d43d72f796b1483dc0e3f
Sanvirus <sannijs.bondars123@gmail.com>
2024-11-10 12:28:01 +0200

    portfolio 001

--- my-app/src/routers/Contact.js
+++ my-app/src/routers/Contact.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Navbar from "../components/Navbar";
+import ContactForm from '../components/ContactForm.js';
 import Footer from "../components/Footer";
 
 const Contact = () => {
     return <div>
         <Navbar />
+        <ContactForm />
         <Footer />
     </div>;
 }
--- my-app/src/routers/Home.js
+++ my-app/src/routers/Home.js
@@ -3,12 +3,14 @@ import Navbar from "../components/Navbar";
 import HeroImg from "../components/HeroImg";
 import Skills from "../components/Skills";
 import Footer from "../components/Footer";
+import AboutSec from "../components/AboutSec";
 
 const Home = () => {
     return <div>
       <Navbar />  
       <HeroImg />
       <Skills />
+      <AboutSec />
       <Footer />
     </div>;
 };
