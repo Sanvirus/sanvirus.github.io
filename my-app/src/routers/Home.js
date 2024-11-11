9e3404ec7e978c0c6c618afdada3c3ea96f55efe
Sanvirus <sannijs.bondars123@gmail.com>
2024-11-11 10:40:18 +0200

    patch

--- my-app/src/routers/Home.js
+++ my-app/src/routers/Home.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import Navbar from '../components/Navbar';
-import HeroImg from '../components/HeroImg';
-import Projects from '../components/Projects'; // New component
-import Skills from '../components/Skills';
-import Footer from '../components/Footer';
-
-const Home = () => {
-    return (
-        <>
-            <Navbar />
-            <HeroImg />
-            <Projects />
-            <Skills />
-            <Footer />
-        </>
-    );
-=======
 import React from "react";
 import Navbar from "../components/Navbar";
 import HeroImg from "../components/HeroImg";
@@ -30,7 +11,6 @@ const Home = () => {
       <Skills />
       <Footer />
     </div>;
->>>>>>> parent of f7733d0 (portfolio 001)
 };
 
 export default Home;
