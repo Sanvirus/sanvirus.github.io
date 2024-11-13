5d55d7eb71cff3a1431a471d2a9277e2b4d0efdd
Sanvirus <sannijs.bondars123@gmail.com>
2024-11-13 08:12:28 +0200

    portfolio done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,14 +8,19 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <div 
+    style={{
+      backgroundColor: '#ffffff',
+      color: '#000000',
+      minHeight: '100vh',
+    }}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project" element={<Project />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
