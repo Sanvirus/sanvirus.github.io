f9a2683cadfb45670d382508a486fc16b3554c66
Sanvirus <sannijs.bondars123@gmail.com>
2024-11-13 11:31:50 +0200

    beta version

---
+++ my-app/src/components/HeroProjects.js
@@ -0,0 +1,19 @@
+import "./HeroProjects.css"
+
+import React, { Component } from 'react'
+
+class HeroImg2 extends Component {
+    render(){
+      return (
+        <div className="hero-img">
+            <div className="heading">
+                <h1>PROJECTS</h1>
+                <p>Some of my most reacent works</p>
+            </div>
+        </div>
+        )  
+    }
+  
+}
+
+export default HeroImg2;
